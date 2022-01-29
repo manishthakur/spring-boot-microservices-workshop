7381fb9ce051a924817626ada6ce2dee08b232dc
Manish Thakur <neelamthakur.in@gmail.com>
2022-01-29 01:00:32 -0500

    Added environment specific profile files.

--- spring-boot-config/src/main/java/io/javabrains/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/io/javabrains/springbootconfig/GreetingController.java
@@ -2,6 +2,7 @@ package io.javabrains.springbootconfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,17 +21,25 @@ public class GreetingController {
     @Value("${my.list.values}")
     private List<String> listValues;
 
-    @Value("#{${db.connection}}")
-    private Map<String, String> dbValues;
+//    @Value("#{${db.connection}}")
+//    private Map<String, String> dbValues;
 
     @Autowired
     private DBSettings dbSettings;
+
+    @Autowired
+    private Environment env;
 
     @GetMapping("/greeting")
     public String greeting() {
         return greetingMessage +
                 "<br/>" + listValues +
-                "<br/>" + dbValues +
+                // "<br/>" + dbValues +
                 "<br/>" + dbSettings;
+    }
+
+    @GetMapping("/envdetails")
+    public String getEnvDetails() {
+        return env.toString();
     }
 }
