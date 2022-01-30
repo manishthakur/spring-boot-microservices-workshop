714430d0ddf9162de59587ef76b220ede794136c
Manish Thakur <manishthakur.in@gmail.com>
2022-01-30 01:20:40 -0500

    Modified changes for spring cloud server config client implementations.

--- spring-boot-config/src/main/java/io/javabrains/springbootconfig/SpringBootConfigApplication.java
+++ spring-boot-config/src/main/java/io/javabrains/springbootconfig/SpringBootConfigApplication.java
@@ -2,12 +2,14 @@ package io.javabrains.springbootconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringBootConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootConfigApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfigApplication.class, args);
+    }
 
 }
