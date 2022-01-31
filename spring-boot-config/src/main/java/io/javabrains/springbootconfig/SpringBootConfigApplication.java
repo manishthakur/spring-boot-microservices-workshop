89cecd4784ccadecb91b246569af49b95df23c0e
Manish Thakur <manishthakur.in@gmail.com>
2022-01-30 23:23:30 -0500

    bug : app not able to read properties from config server Soln. : added required property set in application yaml file

--- spring-boot-config/src/main/java/io/javabrains/springbootconfig/SpringBootConfigApplication.java
+++ spring-boot-config/src/main/java/io/javabrains/springbootconfig/SpringBootConfigApplication.java
@@ -2,10 +2,8 @@ package io.javabrains.springbootconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 public class SpringBootConfigApplication {
 
     public static void main(String[] args) {
